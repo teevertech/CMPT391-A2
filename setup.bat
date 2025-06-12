ca7d19728555059f6e1b5457894d0d014587c147
Joshua Sooley <sooleyj2@mymacewan.ca>
2025-06-12 03:23:06 -0600

    association rules

--- setup.bat
+++ setup.bat
@@ -13,6 +13,9 @@ call venv\Scripts\activate.bat
 REM Upgrade pip and install packages
 python -m pip install --upgrade pip
 pip install pandas
+pip install scikit-learn
+pip install matplotlib
+pip install mlxtend
 
 REM Freeze dependencies
 pip freeze > requirements.txt
